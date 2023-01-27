2d466a9217c4f75dcca8b8197e19aab452bf3516
Jimmy <jimmyjunttila@hotmail.com>
2023-01-27 15:59:31 +0100

    Add 'Models' folder with a 'User.cs' class file

---
+++ WoS-Gen/Models/User.cs
@@ -0,0 +1,21 @@
+﻿namespace WoS_Gen.Models
+{
+    public class User
+    {
+        // Properties for name, email, workoutgoal and location
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string WorkoutGoal { get; set; }
+        public string Location { get; set; }
+
+        // Constructor to initialize user's name, email, workoutgoal and location
+        public User(string name, string email, string workoutGoal, string location)
+        {
+            Name = name;
+            Email = email;
+            WorkoutGoal = workoutGoal;
+            Location = location;
+        }
+
+    }
+}
