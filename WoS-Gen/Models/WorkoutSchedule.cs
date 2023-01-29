4039dd538ea7373d8286f190167eafc3f57b097f
Jimmy <jimmyjunttila@hotmail.com>
2023-01-29 13:01:07 +0100

    Add 'WorkoutSchedule.cs' with 2 arrays/1 dictionary and a 'GenerateSchedule' method

---
+++ WoS-Gen/Models/WorkoutSchedule.cs
@@ -0,0 +1,42 @@
+﻿using WoS_Gen.Components;
+
+namespace WoS_Gen.Models
+{
+    public class WorkoutSchedule
+    {
+        private string[] muscleGroups = { "Chest", "Back", "Legs", "Shoulders", "Arms", "Abs" };
+        private string[] workoutDays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+        public Dictionary<string, string[]> schedule = new Dictionary<string, string[]>();
+        
+        public WorkoutSchedule(User user)
+        {
+            GenerateSchedule(user);
+        }
+        
+        public void GenerateSchedule(User user)
+        {
+            if (user.WorkoutGoal == "Lose Weight")
+            {
+                if (user.Location == "Home")
+                {
+
+                }
+                else if (user.Location == "Gym")
+                {
+
+                }
+            }
+            else if (user.WorkoutGoal == "Build Muscles")
+            {
+                if (user.Location == "Home")
+                {
+
+                }
+                else if (user.Location == "Gym")
+                {
+
+                }
+            }
+        }
+    }
+}
