ff72dab69a30ac9dc98c8ac585af0b2afb9f3c03
Daniel Junttila <daniel_junttila@live.se>
2023-01-30 20:50:59 +0100

    add different workoutplans

--- WoS-Gen/Models/WorkoutSchedule.cs
+++ WoS-Gen/Models/WorkoutSchedule.cs
@@ -6,37 +6,59 @@ namespace WoS_Gen.Models
     {
         private string[] muscleGroups = { "Chest", "Back", "Legs", "Shoulders", "Arms", "Abs" };
         private string[] workoutDays = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-        public Dictionary<string, string[]> schedule = new Dictionary<string, string[]>();
-        
-        public WorkoutSchedule(User user)
+
+        public List<string> GeneratePlan(string workoutGoal, string location)
         {
-            GenerateSchedule(user);
-        }
-        
-        public void GenerateSchedule(User user)
-        {
-            if (user.WorkoutGoal == "LoseWeight")
+            List<string> workoutPlan = new List<string>();
+
+            if (workoutGoal == "LoseWeight")
             {
-                if (user.Location == "Home")
+                if (location == "Home")
                 {
-
+                    workoutPlan.Add("Monday: Cardio (30 minutes)");
+                    workoutPlan.Add("Tuesday: Bodyweight exercises (45 minutes)");
+                    workoutPlan.Add("Wednesday: Rest day");
+                    workoutPlan.Add("Thursday: Cardio (30 minutes)");
+                    workoutPlan.Add("Friday: Bodyweight exercises (45 minutes)");
+                    workoutPlan.Add("Saturday: Rest day");
+                    workoutPlan.Add("Sunday: Yoga (60 minutes)");
                 }
-                else if (user.Location == "Gym")
+                else if (location == "Gym")
                 {
-
+                    workoutPlan.Add("Monday: Cardio (30 minutes)");
+                    workoutPlan.Add("Tuesday: Strength training (45 minutes)");
+                    workoutPlan.Add("Wednesday: Rest day");
+                    workoutPlan.Add("Thursday: Cardio (30 minutes)");
+                    workoutPlan.Add("Friday: Strength training (45 minutes)");
+                    workoutPlan.Add("Saturday: Rest day");
+                    workoutPlan.Add("Sunday: Yoga (60 minutes)");
                 }
             }
-            else if (user.WorkoutGoal == "BuildMuscles")
+            else if (workoutGoal == "BuildMuscles")
             {
-                if (user.Location == "Home")
+                if (location == "Home")
                 {
-
+                    workoutPlan.Add("Monday: Chest and Triceps");
+                    workoutPlan.Add("Tuesday: Back and Biceps");
+                    workoutPlan.Add("Wednesday: Rest day");
+                    workoutPlan.Add("Thursday: Legs and Abs");
+                    workoutPlan.Add("Friday: Shoulders and Arms");
+                    workoutPlan.Add("Saturday: Rest day");
+                    workoutPlan.Add("Sunday: Full-body workout");
                 }
-                else if (user.Location == "Gym")
+                else if (location == "Gym")
                 {
-
+                    workoutPlan.Add("Monday: Chest and Triceps");
+                    workoutPlan.Add("Tuesday: Back and Biceps");
+                    workoutPlan.Add("Wednesday: Rest day");
+                    workoutPlan.Add("Thursday: Legs and Abs");
+                    workoutPlan.Add("Friday: Shoulders and Arms");
+                    workoutPlan.Add("Saturday: Rest day");
+                    workoutPlan.Add("Sunday: Full-body workout");
                 }
             }
+
+            return workoutPlan;
         }
     }
 }
