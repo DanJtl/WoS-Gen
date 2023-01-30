d71fc32c3b2a5d54a482df1aab62b4a02d4aea98
Daniel Junttila <daniel_junttila@live.se>
2023-01-30 19:54:24 +0100

    fix user input for User constructor

--- WoS-Gen/Models/User.cs
+++ WoS-Gen/Models/User.cs
@@ -3,9 +3,18 @@
     public class User
     {
         // Properties for name, email, workoutgoal and location
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-        public string? WorkoutGoal { get; set; }
-        public string? Location { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string WorkoutGoal { get; set; }
+        public string Location { get; set; }
+
+        public User(string userName, string email, string workoutGoal, string location)
+        {
+            Name = userName;
+            Email = email;
+            WorkoutGoal = workoutGoal;
+            Location = location;
+        }
     }
+    
 }
